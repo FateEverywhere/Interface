c3c00638472dfc2f6059cb49af61ef5883890c7a
FateEverywhere <41215461+FateEverywhere@users.noreply.github.com>
2022-03-27 16:29:46 -0400

    Added Fate keymap to HL-Mk3

---
+++ Current/Firmware/hardlight/mk3/keymaps/fate/config.h
@@ -0,0 +1,11 @@
+#undef  LEADER_TIMEOUT
+#define LEADER_TIMEOUT 250
+#define LEADER_PER_KEY_TIMING
+
+#pragma once
+
+// place overrides here
+
+/* Change USB Polling Rate to 1000hz and a larger keys per scan for elite gaming*/
+#define USB_POLLING_INTERVAL_MS 1
+#define QMK_KEYS_PER_SCAN 12
